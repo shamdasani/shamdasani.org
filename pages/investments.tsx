050304d21caf9c983b59bc30a3527008b4094c4c
Samay Shamdasani <techboss302@gmail.com>
2024-10-02 21:03:47 -0400

    .

--- pages/investments.tsx
+++ pages/investments.tsx
@@ -117,6 +117,26 @@ const Projects = () => {
             </a>{" "}
             - warm outbound platform (2024)
           </div>
+          <div className="my-2 text-gray-900">
+            <a
+              target="_blank"
+              className="underline hover:opacity-75 text-xl"
+              href="https://www.unifygtm.com/"
+            >
+              Revin
+            </a>{" "}
+            - AI for home services (2024)
+          </div>
+          <div className="my-2 text-gray-900">
+            <a
+              target="_blank"
+              className="underline hover:opacity-75 text-xl"
+              href="https://www.unifygtm.com/"
+            >
+              Endeavor
+            </a>{" "}
+            - AI for manufacturing (2024)
+          </div>
           <p className="text-sm text-gray-600 italic mt-4">
             My favorite part of being in the technology industry is meeting so
             many smart and ambitious people. I've been lucky enough to invest in
